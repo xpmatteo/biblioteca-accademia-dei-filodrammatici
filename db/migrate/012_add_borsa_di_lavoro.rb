e760bbbe28138fcd514212eaf7e24633c8e23efb
Matteo Vaccari <vaccari@pobox.com>
2006-09-05 10:56:45 +0000

    git-svn-id: svn+ssh://matteo.textdriven.com/home/matteo/svn/filo/trunk@35 5c65d9dd-4713-0410-ba8d-d7aa5c880541

--- db/migrate/012_add_borsa_di_lavoro.rb
+++ db/migrate/012_add_borsa_di_lavoro.rb
@@ -1,8 +1,9 @@
 class AddBorsaDiLavoro < ActiveRecord::Migration
   def self.up
-    Content.new( :title => 'Borsa di lavoro "Antonio Marietti"', :name => '/borsa-antonio-marietti', :body => 'bla bla' ).save
+    Content.new( :title => 'Borsa di lavoro "Antonio Marietti"', :name => 'borsa-antonio-marietti', :body => 'bla bla' ).save
   end
 
   def self.down
+    Content.delete(:conditions => "name like '/%'")
   end
 end
