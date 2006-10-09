1e2acd92fcfa64ea041d23abd39de6e9a9259b85
Matteo Vaccari <vaccari@pobox.com>
2006-10-09 08:28:13 +0000

    git-svn-id: svn+ssh://matteo.textdriven.com/home/matteo/svn/filo/trunk@55 5c65d9dd-4713-0410-ba8d-d7aa5c880541

--- app/models/content.rb
+++ app/models/content.rb
@@ -4,7 +4,7 @@ class Content < ActiveRecord::Base
   validates_presence_of :title, :name, :body
   
   file_column :image, 
-    :magick => { :geometry => "1000x98>" },
+    :magick => { :geometry => "1000x200>" },
     :root_path => File.join(RAILS_ROOT, "public", "upload"), 
     :web_root => "upload/"
 end
