abb2cdb8d91026b086f1de5f230324cb1c06c011
Matteo Vaccari <vaccari@pobox.com>
2006-10-09 15:40:08 +0000

    git-svn-id: svn+ssh://matteo.textdriven.com/home/matteo/svn/filo/trunk@57 5c65d9dd-4713-0410-ba8d-d7aa5c880541

--- test/integration/import_from_unimarc_test.rb
+++ test/integration/import_from_unimarc_test.rb
@@ -2,6 +2,13 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class DocumentTest < Test::Unit::TestCase
 
+  # todo:
+  # eliminare i campi derivati da Document
+  # author deve diventare ??? cosa ???
+  # relazione multiforme fra documenti e persone: autore, editore, o anche non specificata
+  # la relazione pure va calcolata al volo ?
+  
+
   def setup
     Document.delete_all
     Author.delete_all
@@ -34,25 +41,49 @@ class DocumentTest < Test::Unit::TestCase
     assert_equal 'Monografia', d.bibliographic_level
     assert_equal '111 p. : ill. ; 17 cm', d.physical_description
     assert_equal 'Testo a stampa', d.document_type
+    assert_equal 'Mor, Carlo A.', d.authors
+    assert_equal ['Mor, Carlo A.'], d.names.map {|n| n.name}
   end
   
-  
-
   def test_import_complex_book_acceptance
     d = Document.find_by_id_sbn('IT\ICCU\AQ1\0031672')
     assert_equal 'Italiano', d.language
+    assert_equal 'Monografia', d.bibliographic_level
+    assert_equal 'Testo a stampa', d.document_type
+    assert_equal 'Teatro elisabettiano / Kyd, Marlowe, Heywood, Marston, Jonson, Webster, Tourneur, Ford', d.title
+    assert_equal d.title, d.title_without_article
+    assert_equal 'Firenze : Sansoni, stampa 1954', d.publication
+    assert_equal 'XXVIII, 1274 p. ; 21 cm', d.physical_description
 
-    assert_equal 'Teatro elisabettiano / Kyd, Marlowe, Heywood, Marston, Jonson, Webster, Tourneur, Ford', d0.title
-    assert_equal d0.title, d0.title_without_article
-    assert_equal 'Firenze', d0.place_of_publication
-    assert_equal 'Sansoni', d0.publisher
-    assert_equal 'stampa 1954', d0.date_of_publication
-
-    assert_equal 'Praga, Marco', d2.author.name
-    assert_nil d0.author
-
-    assert_equal "L' Adargonte tragedia del signor Prospero Mandosi nobile romano, e caualiere di S. Stefano", d3.title 
-    assert_equal "Adargonte tragedia del signor Prospero Mandosi nobile romano, e caualiere di S. Stefano", d3.title_without_article
+    # Collezione : 	I grandi classici stranieri
+    assert_equal 'Trad. di Gabriele Baldini ... [et al.!, sotto la direzione di Mario Praz',
+      d.general_notes
+    
+    assert_equal ['Praz, Mario', 'Baldini, Gabriele'], d.names.map {|n| n.name}
+    assert_equal 'Italia', d.publication_country
+    	
+    assert_equal 'Firenze', d.place_of_publication
+    assert_equal 'Sansoni', d.publisher
+    assert_equal 'stampa 1954', d.date_of_publication
   end  
-  
+
+  def test_import_title_with_asterisk_acceptance
+    d = Document.find_by_id_sbn('IT\ICCU\BVEE\023209')
+    assert_equal 'Monografia', d.bibliographic_level
+    assert_equal 'Testo a stampa', d.document_type
+    assert_equal "L' Adargonte tragedia del signor Prospero Mandosi nobile romano, e caualiere di S. Stefano", d.title 
+    assert_equal "Adargonte tragedia del signor Prospero Mandosi nobile romano, e caualiere di S. Stefano", d.title_without_article
+    assert_equal 'Mandosio, Prospero <1650-1709>', d.authors
+    assert_equal 'In Bologna : per Giuseppe Longhi, 1687', d.publication
+    assert_equal '120 p. ; 12o.', d.physical_description
+    assert_equal 'Segn.: A-E12.', d.general_notes
+    assert_equal 'Segn.: A-E12.', d.general_notes
+    assert_equal '- roi- o.e. .)di (Gpo (3) 1687 (R)', d.fingerprint
+
+    # ???
+    # Nomi: 	Mandosio, Prospero <1650-1709>
+    # [Editore] Longhi, Giuseppe <1.> 	
+
+    assert_equal 'Italia', d.publication_country
+  end
 end
