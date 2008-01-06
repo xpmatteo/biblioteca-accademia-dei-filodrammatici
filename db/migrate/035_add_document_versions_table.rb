ebe2c3587b75674422eb9a35d1dd4d247e06af89
Matteo Vaccari <vaccari@pobox.com>
2008-01-06 17:54:55 +0000

    fix problema versionamento
    git-svn-id: svn+ssh://matteo.textdriven.com/home/matteo/svn/filo/trunk@145 5c65d9dd-4713-0410-ba8d-d7aa5c880541

--- db/migrate/035_add_document_versions_table.rb
+++ db/migrate/035_add_document_versions_table.rb
@@ -1,15 +1,19 @@
 class AddDocumentVersionsTable < ActiveRecord::Migration
   def self.up
+    self.down
     Document.create_versioned_table
-    Document.find(:all).each do |d|
-      @count ||= 0
-      @count += 1
-      puts @count if @count % 100 == 0
-      d.save
+    
+    page = 100
+    (0..130).each do |offset|
+      Document.find(:all, :limit => page, :offset => offset*page).each do |d|
+        d.save
+      end
+      puts page*(offset+1)
     end
   end
 
   def self.down
     Document.drop_versioned_table
+  rescue
   end
 end
