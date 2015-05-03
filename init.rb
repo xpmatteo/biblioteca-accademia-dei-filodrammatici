3ed5f4566af8728ed0925cef441d3a6ebbd52abc
Matteo Vaccari <mvaccari@thoughtworks.com>
2015-05-03 16:15:42 +0200

    Update to Sinatra 1.4.6

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
-require 'sinatra/base'
+require 'sinatra'
 
 require './app/helpers/application_helper.rb'
-Dir.glob('./app/{models,helpers,controllers}/*.rb').sort.each { |file| 
+Dir.glob('./app/{models,helpers,controllers}/*.rb').sort.each { |file|
   puts file
-  require file 
+  require file
 }
