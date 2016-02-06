7575ffea669d0995636947b316e82a166b1fa85d
Matteo Vaccari <mvaccari@thoughtworks.com>
2016-02-06 17:43:18 +0100

    Ensure rvm is loaded correctly at cd time

--- script/run-production.sh
+++ script/run-production.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-set -e
-cd "$(dirname "$0")/.."
 
 source "/home/matteo/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+set -e
+cd "$(dirname "$0")/.."
 
 export LANG=en_US.UTF-8
 
